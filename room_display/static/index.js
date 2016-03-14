54cecd8146f0bef76e766c805199dd591dff9686
Nicholas Lee <csudcy@gmail.com>
2016-03-14 12:31:55 +0000

    Got time working

--- room_display/static/index.js
+++ room_display/static/index.js
@@ -1,17 +1,25 @@
 angular.module('roomDisplayApp', [])
-  .controller('RoomDisplayController', function() {
-    var roomDisplay = this;
-    roomDisplay.rooms = [
-        {name: 'A room'},
-        {name: 'Another room'}
-    ];
-});
+    .controller('RoomDisplayController', function() {
+        var roomDisplay = this;
+        roomDisplay.rooms = [
+            {name: 'A room'},
+            {name: 'Another room'}
+        ];
+    }).controller('TimeController', function($scope, $interval) {
+        // Ticking Clock
+        // http://stackoverflow.com/questions/23383233/how-to-make-a-ticking-clock-time-in-angularjs-and-html
+        var tick = function() {
+            $scope.clock = Date.now();
+        }
+        tick();
+        $interval(tick, 1000);
+    });
 
 
-document.getElementById('fullscreen').onclick = function() {
-    var req = document.body.requestFullScreen || document.body.webkitRequestFullScreen || document.body.mozRequestFullScreen;
-    req.call(document.body.parentNode);
-};
+// document.getElementById('fullscreen').onclick = function() {
+//     var req = document.body.requestFullScreen || document.body.webkitRequestFullScreen || document.body.mozRequestFullScreen;
+//     req.call(document.body.parentNode);
+// };
 
 // Show the page name in the dropdown
 $('#page_changer').on('hidden.bs.dropdown', update_page_name);

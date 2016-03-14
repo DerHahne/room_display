aff8946e94ee63a5e99755d1fe4bc082228eab82
Nicholas Lee <csudcy@gmail.com>
2016-03-14 11:58:42 +0000

    Added dropdown nav

--- room_display/static/index.js
+++ room_display/static/index.js
@@ -12,3 +12,16 @@ document.getElementById('fullscreen').onclick = function() {
     var req = document.body.requestFullScreen || document.body.webkitRequestFullScreen || document.body.mozRequestFullScreen;
     req.call(document.body.parentNode);
 };
+
+// Show the page name in the dropdown
+$('#page_changer').on('hidden.bs.dropdown', update_page_name);
+
+function update_page_name() {
+    var page_name = $('#page_changer li.active').text();
+    $('#page_name').text(page_name);
+}
+
+$(document).ready(function() {
+    //Show the current page name
+    update_page_name();
+});

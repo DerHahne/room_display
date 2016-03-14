1be4917dcd3998e5c806cf213d959e42ab100643
Michael Hinstridge <rangeroffire@gmail.com>
2016-03-14 16:49:36 +0000

    Working ish version including excessively long lines and monkey patching :)

--- room_display/app.py
+++ room_display/app.py
@@ -1,10 +1,16 @@
 import os
+from datetime import datetime
 
 import flask
-from flask import Flask
-from flask.ext.heroku import Heroku
-from flask.ext.script import Manager
-from flask.ext.script import Server
+from flask import Flask, jsonify
+#from flask.ext.heroku import Heroku
+from flask_heroku import Heroku
+#from flask.ext.script import Manager
+from flask_script import Manager
+#from flask.ext.script import Server
+from flask_script import Server
+
+from service.exchange import ExchangeCalendar
 
 
 ##################################################
@@ -16,6 +22,31 @@ app.debug = True
 heroku = Heroku(app)
 
 
+config = {
+    'domain': os.environ.get('OUTLOOK_DOMAIN'),
+    'ews_url': os.environ.get('OUTLOOK_EWS_URL'),  # EWS = Exchange Web Services
+    'username': os.environ.get('OUTLOOK_USERNAME'),
+    'password': os.environ.get('OUTLOOK_PASSWORD'),
+    'poll_interval': os.environ.get('OUTLOOK_POLL_INTERVAL', 1),
+    'poll_start_minute': os.environ.get('OUTLOOK_POLL_START_MINUTE', 420),
+    'poll_end_minute': os.environ.get('OUTLOOK_POLL_END_MINUTE', 1140),
+}
+
+
+def _transform_booking_info(booking):
+    start = booking.pop('start')
+    end = booking.pop('end')
+    booking.pop('description')
+
+    booking['start_minute'] = start.hour * 60 + start.minute
+    booking['end_minute'] = end.hour * 60 + end.minute
+
+    return booking
+
+exchange = ExchangeCalendar(config['domain'], config['ews_url'], config['username'], config['password'])
+
+MEETING_ROOMS = {room['displayName']: room['email'] for room in exchange.get_contacts('-MR')}
+
 @app.route('/')
 def hello_world():
     return flask.send_file('./templates/index.html')
@@ -23,7 +54,29 @@ def hello_world():
 
 @app.route('/data')
 def data():
-    return flask.send_file('../example_data.json')
+    start = datetime.today().replace(hour=7, minute=0, second=0, microsecond=0)
+    end = datetime.today().replace(hour=23, minute=0, second=0, microsecond=0)
+    id_namespace = ".".join(reversed(config['ews_url'].split('//', 1)[1].split('/')[0].split('.')))
+
+    ret = {
+        "polling": {
+            "interval": config['poll_interval'],
+            "start_minute": config['poll_start_minute'],
+            "end_minute": config['poll_end_minute'],
+        },
+        "rooms": [],
+    }
+
+    for room_name, room_email in MEETING_ROOMS.iteritems():
+        meeting_room_details = {
+            "id": "{}.{}".format(id_namespace, room_email.split('@')[0]),
+            "name": room_name,
+            "description": None,
+            "bookings": [_transform_booking_info(booking) for booking in exchange.get_bookings(start, end, room_email)]
+        }
+        ret["rooms"].append(meeting_room_details)
+
+    return jsonify(ret)
 
 
 ##################################################
---
+++ room_display/service/exchange.py
@@ -0,0 +1,89 @@
+from pytz import timezone
+
+from pyexchange import Exchange2010Service, ExchangeNTLMAuthConnection
+from pyexchange.exceptions import (FailedExchangeException, ExchangeStaleChangeKeyException,
+                                   ExchangeItemNotFoundException, ExchangeIrresolvableConflictException,
+                                   ExchangeInternalServerTransientErrorException)
+from pyexchange.exchange2010 import Exchange2010CalendarEvent
+from pyexchange.exchange2010 import soap_request
+
+from .base import BaseCalendar
+
+
+# A monkey patching we do go
+def non_borked_check_for_exchange_fault(self, xml_tree):
+    response_codes = xml_tree.xpath(u'//m:ResponseCode', namespaces=soap_request.NAMESPACES)
+
+    if not response_codes:
+      raise FailedExchangeException(u"Exchange server did not return a status response", None)
+
+    # The full (massive) list of possible return responses is here.
+    # http://msdn.microsoft.com/en-us/library/aa580757(v=exchg.140).aspx
+    for code in response_codes:
+      if code.text == u"ErrorChangeKeyRequiredForWriteOperations":
+        # change key is missing or stale. we can fix that, so throw a special error
+        raise ExchangeStaleChangeKeyException(u"Exchange Fault (%s) from Exchange server" % code.text)
+      elif code.text == u"ErrorItemNotFound":
+        # exchange_invite_key wasn't found on the server
+        raise ExchangeItemNotFoundException(u"Exchange Fault (%s) from Exchange server" % code.text)
+      elif code.text == u"ErrorIrresolvableConflict":
+        # tried to update an item with an old change key
+        raise ExchangeIrresolvableConflictException(u"Exchange Fault (%s) from Exchange server" % code.text)
+      elif code.text == u"ErrorInternalServerTransientError":
+        # temporary internal server error. throw a special error so we can retry
+        raise ExchangeInternalServerTransientErrorException(u"Exchange Fault (%s) from Exchange server" % code.text)
+      elif code.text == u"ErrorCalendarOccurrenceIndexIsOutOfRecurrenceRange":
+        # just means some or all of the requested instances are out of range
+        pass
+      elif code.text == u"ErrorNameResolutionMultipleResults":
+        # This just means that we got more than one possible contact
+        pass
+      elif code.text != u"NoError":
+        raise FailedExchangeException(u"Exchange Fault (%s) from Exchange server" % code.text)
+
+Exchange2010Service._check_for_exchange_fault = non_borked_check_for_exchange_fault
+
+
+class ExchangeCalendar(BaseCalendar):
+    def __init__(self, domain, url, username, password):
+        super(ExchangeCalendar, self).__init__()
+
+        connection = ExchangeNTLMAuthConnection(url=url,
+                                                username='%s\\%s' % (domain, username),
+                                                password=password)
+        self._service = Exchange2010Service(connection)
+
+    def get_calendar(self):
+        return self._service.calendar()
+
+    def get_bookings(self, start, end, email_address):
+        try:
+            return [
+                self.calendar_event_to_dict(event)
+                for event in
+                self.calendar.list_events(
+                    start=timezone("GMT").localize(start),
+                    end=timezone("GMT").localize(end),
+                    details=False,
+                    delegate_for=email_address
+                ).events
+            ]
+        except FailedExchangeException as ex:
+            raise RuntimeError(str(ex))
+
+    @staticmethod
+    def calendar_event_to_dict(event):
+        if not isinstance(event, Exchange2010CalendarEvent):
+            raise ValueError('{} is not of type {}'.format(type(event), type(Exchange2010CalendarEvent)))
+
+        return {
+            'username': event.organizer.name,
+            'email_address': event.organizer.email,
+            'subject': event.subject.strip(),
+            'description': event.text_body,
+            'start': event.start,
+            'end': event.end,
+        }
+
+    def get_contacts(self, search):
+        return self._service.contacts().search_contacts(search).contacts
